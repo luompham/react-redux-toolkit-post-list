690fae4c49794e188c90da23721e9c71b4467311
Luom Pham <luompham95@gmail.com>
2023-04-26 06:31:57 +0700

    updated TimeAgo

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -3,28 +3,31 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { PostAuthor } from './PostAuthor';
+import { TimeAgo } from './TimeAgo';
 
 export const PostList = () => {
 
     const posts = useSelector(state => state.posts);
 
-
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+    const rederedPosts = orderedPosts.map(post => {
+        return (
+            <article className='post-excerpt' key={post.id}>
+                <h3> {post.title}</h3>
+                <div>
+                    <PostAuthor userId={post.user} />
+                    <TimeAgo timestamp={post.date} />
+                </div>
+                <p className="post-content"> {post.content.substring(0, 100)}</p>
+                <Link to={`/posts/${post.id}`} className="button muted-button">View Post</Link>
+            </article>
+        );
+    })
     return (
         <section className='posts-list'>
             <h2>Posts</h2>
-            {posts.map((post) => {
-                return (
-                    <article className='post-excerpt' key={post.id}>
-                        <h3> {post.title}</h3>
-                        <div>
-                            <PostAuthor userId={post.user} />
-                        </div>
-                        <p> {post.content.substring(0, 100)}</p>
-                        <Link to={`/posts/${post.id}`} className="button muted-button">View Post</Link>
-                    </article>
-                )
-            })}
-
+            {rederedPosts}
         </section>
     )
 }
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,10 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
 
 
 const initialState = [
-    { id: 1, title: 'Fist Post', content: 'Hello!' },
-    { id: 2, title: 'Second Post', content: 'More text' },
+    {
+        id: 1,
+        title: 'Fist Post',
+        content: 'Hello!',
+        date: sub(new Date(), { minutes: 10 }).toISOString()
+    },
+    {
+        id: 2,
+        title: 'Second Post',
+        content: 'More text',
+        date: sub(new Date(), { minutes: 5 }).toISOString()
+    },
 ];
 
 const postSlice = createSlice(
