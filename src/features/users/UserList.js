02aa5aff79aab3497d946df6c220876c454f5a73
luompham <luompham95@gmail.com>
2023-05-16 16:29:58 +0700

    updated user list

--- src/features/users/UserList.js
+++ src/features/users/UserList.js
@@ -6,13 +6,20 @@ import { selectAllUsers } from './usersSlice'
 export const UserList = () => {
 
     const userList = useSelector(selectAllUsers);
-    console.log(userList);
+
+    const renderedUsers = userList.map(user => {
+        return (
+            <li key={user.id}>
+                <Link to={`/users/${user.id}`}>{user.name}</Link>
+            </li>  
+        )   
+    })
 
     return (
         <section>
             <h2>Users</h2>
             <ul>
-
+                {renderedUsers}
             </ul>
         </section>
     )
